4a8a9037d9b297fd811ebe83c4260ceb46db1e7f
rust_lang_lover <rust_lang_lover@proton.me>
2024-05-21 13:26:38 +0300

    // added fab for fast top scroll

--- src/js/scripts/visual_activator.js
+++ src/js/scripts/visual_activator.js
@@ -2,4 +2,11 @@
 $(function() {
     AOS.init();
     new WOW().init();
+
+    // For a smooth scroll to the top of the page.
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop : $(_href).offset().top+"px"});
+        return false
+    });
 })
