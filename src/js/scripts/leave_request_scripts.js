811ff230de260c95e4bd714fba59344755cc6f30
rust_lang_lover <rust_lang_lover@proton.me>
2024-05-22 15:10:34 +0300

    // added check for form before posting + created constans.js file for routes

--- src/js/scripts/leave_request_scripts.js
+++ src/js/scripts/leave_request_scripts.js
@@ -1,5 +1,40 @@
+function verify_fields(name : String, phone_mail : String, comment : String) : [boolean, String] {
+    if (name.length >= 2) {
+        if (phone_mail.includes("@")) {
+            if (comment.length >= 10) {
+                return [true, ""]
+            }
+            else {
+                return [false, "Поле для комментария должно содержать более 9 символов."]
+            }
+        }
+        else {
+            if (phone_mail.length >= 7) {
+                if (comment.length >= 10) {
+                    return [true, ""]
+                }
+                else {
+                    return [false, "Поле для комментария должно содержать более 9 символов."]
+                }
+            }
+            else {
+                return [false, "Ваша почта/телефон некорректно заполнены. Если вводили почту - проверьте наличие @. Если номер - длина номера должна быть больше 6 символов."]
+            }
+        }
+    }
+    else {
+        return [false, "Поле имя должно быть длинее 1 символа"]
+    }
+}
+
+function write_customer_request(name : String, phone : String, comment : String) : void {
+    fetch()
+}
+
 $(function() {
-    $(".leave-request-div__request-frame-div__button").on({
+    const button = $(".leave-request-div__request-frame-div__button");
+
+    button.on({
         mouseenter : function() {
             $(this).css({
                 'background-color' : "rgb(174, 209, 84)",
@@ -15,8 +50,16 @@ $(function() {
     });
 
     // Fetch the name & surname of customer and then make a request to the API.
-    $(".leave-request-div__request-frame-div__button").on('click', function() {
+    button.on('click', function() {
         const name = document.getElementById("customer_name").value;
         const phone_num = document.getElementById("customer_phone").value;
+        const comment = document.getElementById("customer_comment").value;
+
+        switch (verify_fields(name, phone_num, comment)[0]) {
+            case true:
+                return
+            case false:
+                return;
+        }
     })
 });
