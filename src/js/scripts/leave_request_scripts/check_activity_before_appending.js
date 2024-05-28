2dcfe765383aabb7eb57a08288fb2f6a03eeb609
rust_lang_lover <rust_lang_lover@proton.me>
2024-05-28 15:21:30 +0300

    // added check_activity_before_appending.js script for fixing issue with message windows

---
+++ src/js/scripts/leave_request_scripts/check_activity_before_appending.js
@@ -0,0 +1,10 @@
+function check_activity_before_appending() {
+    if (document.querySelector('.leave-request-div__result_message_form') === null) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+export default check_activity_before_appending;
