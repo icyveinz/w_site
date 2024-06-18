23192ae801ee49b12ce3a7d13640a7989a183906
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-18 11:57:12 +0300

    // half part work menu

--- src/js/scripts/main/burger_menu/burger_menu.js
+++ src/js/scripts/main/burger_menu/burger_menu.js
@@ -1,11 +1,15 @@
 import extract_menu_template from "./extract_menu_template.js";
+import extract_root_container from "./extract_root_container.js";
 
 $(function() {
     let condition = false
     $('.burger').on('click', function() {
         console.log('button is clicked');
         if (!condition) {
-            $('.burger-menu-core__unwrapped-and-centered-container').append(extract_menu_template());
+            $('.burger-menu-core').append(extract_root_container());
+            setTimeout(() => {
+                $('.burger-menu-core__unwrapped-and-centered-container').append(extract_menu_template());
+            }, 900);
             condition = !condition;
         }
         else {
