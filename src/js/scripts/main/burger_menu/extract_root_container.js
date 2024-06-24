7454b5523dfc20ed2794abee09145a85225b730c
rust_lang_lover <rust_lang_lover@proton.me>
2024-06-24 12:41:14 +0300

    // comments

--- src/js/scripts/main/burger_menu/extract_root_container.js
+++ src/js/scripts/main/burger_menu/extract_root_container.js
@@ -1,3 +1,5 @@
+// return menu root template for future actions
+
 function extract_root_container() {
     return `<nav class="burger-menu-core__unwrapped-and-centered-container animate__animated animate__fadeInDown"></nav>`
 }
