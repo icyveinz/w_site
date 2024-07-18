0f3a5ca1a1301ce37d655470c1b6703d78195fda
Egor Ivanov <rust_lang_lover@proton.me>
2024-07-18 11:43:29 +0300

    // additional comments

--- src/js/scripts/main/leave_request_scripts/check_activity_before_appending.js
+++ src/js/scripts/main/leave_request_scripts/check_activity_before_appending.js
@@ -1,3 +1,4 @@
+// Check if window is already at the screen
 function check_activity_before_appending() {
     if (document.querySelector('.leave-request-div__result_message_form') === null) {
         return true
