febd3aceeeeb01b549a8016ac1ac79b1e15b6b64
rust_lang_lover <rust_lang_lover@proton.me>
2024-07-30 12:43:39 +0300

    // removed unused code from gsap_legal_animation.js

--- src/js/scripts/legal/gsap_legal_animation.js
+++ src/js/scripts/legal/gsap_legal_animation.js
@@ -1,15 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // const li_elements = document.querySelectorAll('.agreement-public-container__section__unordered-list__li');
-    // const timeline = gsap.timeline();
-    // li_elements.forEach((element) => {
-    //     gsap.from(element, {
-    //         opacity: 0,
-    //         yPercent: -10,
-    //         ease: "expo.in",
-    //         duration: 2
-    //     });
-    // });
-
     const timeline = gsap.timeline();
     timeline
         .from('.return-to-main-centered__restricted-width__a', {
