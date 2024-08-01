9fd8b2e8123eb479ff766a0879a18c28390c74df
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-01 14:37:31 +0300

    // added new animation

---
+++ src/js/scripts/main/leave_request_scripts/gsap_leave_request.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const request_line = gsap.timeline();
+    request_line
+        .from('.leave-request-div__h1-title-styler', {
+            opacity: 0,
+            yPercent: -15,
+            duration: 1
+        })
+        .from('.leave-request-div__p-title-styler', {
+            opacity: 0,
+            xPercent: -10,
+            duration: 2
+        })
+        .from('.leave-request-div__request-call-column-align', {
+            opacity: 0,
+            duration: 1,
+            transform: 'scale(0)'
+        });
+});
