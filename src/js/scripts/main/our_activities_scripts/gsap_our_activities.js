91639199c24016307e346b0102563a30b934874a
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-01 14:53:47 +0300

    // added new animation for country flags

---
+++ src/js/scripts/main/our_activities_scripts/gsap_our_activities.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+document.addEventListener('DOMContentLoaded', () => {
+    const country_node = document.querySelectorAll('.our-activities-core__countries-layout-container__image-comment-container');
+    country_node.forEach((element) => {
+        const country_timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: element,
+                start: 'top center',
+                scrub: true,
+                end: 'bottom center'
+            }
+        });
+        country_timeline
+            .from(element.querySelector('img'), {
+                opacity: 0,
+                yPercent: -10,
+                transform: 'scale(0)',
+                duration: 2
+            })
+            .from(element.querySelector('span'), {
+                opacity: 0,
+                fontSize: 0,
+                duration: 1
+            })
+    })
+});
