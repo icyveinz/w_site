03047af94f0222ea59c9dd625bfb3bdabc2bb947
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-01 16:12:46 +0300

    // animation fix pros

--- src/js/scripts/main/what_u_get_scripts/gsap_what_u_get.js
+++ src/js/scripts/main/what_u_get_scripts/gsap_what_u_get.js
@@ -1,31 +1,32 @@
 gsap.registerPlugin(ScrollTrigger);
 document.addEventListener('DOMContentLoaded', () => {
-    const pros_node = document.querySelectorAll('.what-u-get-core__pros-container__single-layout');
-    pros_node.forEach((element) => {
-        const pros_timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: element,
-                start: 'top center',
-                scrub: true,
-                end: 'bottom center',
-                markers: true
-            }
-        });
-        pros_timeline
-            .from(element.querySelector('.what-u-get-core__pros-container__single-layout__image-container'), {
-                opacity: 0,
-                yPercent: -20,
-                duration: 2
-            })
-            .from(element.querySelector('.what-u-get-core__pros-container__single-layout__title-comment-container__title'), {
-                xPercent: 20,
-                opacity: 0,
-                duration: 2
-            })
-            .from(element.querySelector('.what-u-get-core__pros-container__single-layout__title-comment-container__comment'), {
-                xPercent: 20,
-                opacity: 0,
-                duration: 1
-            })
-    })
+    const clipping_node = document.querySelector('.what-u-get-core__restricted-width');
+    const pros_timeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: clipping_node,
+            start: 'top center',
+            scrub: true,
+            end: 'bottom bottom',
+            markers: true
+        }
+    });
+    pros_timeline
+        .from(document.querySelectorAll('.what-u-get-core__pros-container__single-layout__image-container'), {
+            opacity: 0,
+            yPercent: -20,
+            duration: 2,
+            stagger: 0.2
+        })
+        .from(document.querySelectorAll('.what-u-get-core__pros-container__single-layout__title-comment-container__title'), {
+            xPercent: 20,
+            opacity: 0,
+            duration: 2,
+            stagger: 0.2
+        })
+        .from(document.querySelectorAll('.what-u-get-core__pros-container__single-layout__title-comment-container__comment'), {
+            xPercent: 20,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.2
+        })
 });
