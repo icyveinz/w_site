0029729534644b6774bcbc9e5e7b6f0444d4768b
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-06 14:38:56 +0300

    // added cleaner for js files

---
+++ cleaner.js
@@ -0,0 +1,25 @@
+var glob = require('glob');
+var fs = require('fs');
+
+const working_directories = ['dist/js/scripts/main/**/*.js', 'dist/js/scripts/legal/*.js']
+
+working_directories.forEach((directory) => {
+    glob(directory, (err, files) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        files.forEach((value) => {
+            if (value.includes('-min')) {
+                fs.rename(value, value.replace("-min", ""), (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
+            }
+            else {
+                fs.unlinkSync(value);
+            }
+        })
+    })
+})
