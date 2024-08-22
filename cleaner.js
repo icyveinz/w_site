f146f10870424306784d16d2f5dd826393124a66
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-22 13:27:50 +0300

    // refactored cleaner.js + webpack.config.js

--- cleaner.js
+++ cleaner.js
@@ -3,20 +3,6 @@ var fs = require('fs')
 
 const working_directories = ['dist/js/scripts/main', 'dist/js/scripts/legal']
 
-// working_directories.forEach((directory) => {
-//     glob(directory, (err, files) => {
-//         if (err) {
-//             console.log(err);
-//             process.exit(1);
-//         }
-//         files.forEach((value) => {
-//             if (!value.includes('bundled')) {
-//                 fs.unlinkSync(value);
-//             }
-//         })
-//     })
-// })
-
 working_directories.forEach((directory) => {
     console.log(directory)
     console.log(fs.readdirSync(directory))
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,31 @@
 const path = require("path");
+
 module.exports = {
-    entry: [
-        "./src/js/scripts/main/preloader/preloader.js",
-        "./src/js/scripts/main/visiual_init/visual_activator.js",
-        "./src/js/scripts/main/fixed_menu_scripts/fixed_menu_scripts.js",
-        "./src/js/scripts/main/burger_menu/burger_menu.js",
-        "./src/js/scripts/main/burger_menu/hover_animation_burger_menu.js",
-        "./src/js/scripts/main/leave_request_scripts/leave_request_scripts.js",
-        "./src/js/scripts/main/leave_request_scripts/gsap_leave_request.js",
-        "./src/js/scripts/main/gsap_media/gsap_media_container.js",
-        "./src/js/scripts/main/our_services_scripts/gsap_our_services.js",
-        "./src/js/scripts/main/what_u_get_scripts/gsap_what_u_get.js",
-        "./src/js/scripts/main/our_products_scripts/gsap_our_products.js",
-        "./src/js/scripts/main/our_products_scripts/our_products_hover_redirect.js",
-        "./src/js/scripts/main/our_reliability_scripts/our_reliability_scripts.js"
-    ],
+    entry: {
+        main: [
+            "./src/js/scripts/main/preloader/preloader.js",
+            "./src/js/scripts/main/visiual_init/visual_activator.js",
+            "./src/js/scripts/main/fixed_menu_scripts/fixed_menu_scripts.js",
+            "./src/js/scripts/main/burger_menu/burger_menu.js",
+            "./src/js/scripts/main/burger_menu/hover_animation_burger_menu.js",
+            "./src/js/scripts/main/leave_request_scripts/leave_request_scripts.js",
+            "./src/js/scripts/main/leave_request_scripts/gsap_leave_request.js",
+            "./src/js/scripts/main/gsap_media/gsap_media_container.js",
+            "./src/js/scripts/main/our_services_scripts/gsap_our_services.js",
+            "./src/js/scripts/main/what_u_get_scripts/gsap_what_u_get.js",
+            "./src/js/scripts/main/our_products_scripts/gsap_our_products.js",
+            "./src/js/scripts/main/our_products_scripts/our_products_hover_redirect.js",
+            "./src/js/scripts/main/our_reliability_scripts/our_reliability_scripts.js"
+        ],
+        legal: [
+            "./src/js/scripts/main/preloader/preloader.js",
+            "./src/js/scripts/legal/gsap_legal_animation.js"
+        ]
+    },
     output: {
-        filename: 'bundled_main.js',
-        path: path.resolve(__dirname, 'dist/js/scripts/main'),
+        filename: 'bundled_[name].js',
+        path: path.resolve(__dirname, 'dist/js/scripts'),
     }
 };
-
-// module.exports = {
-//     entry: [
-//         "./src/js/scripts/main/preloader/preloader.js",
-//         "./src/js/scripts/legal/gsap_legal_animation.js"
-//     ],
-//     output: {
-//         filename: 'bundled_legal.js',
-//         path: path.resolve(__dirname, 'dist/js/scripts/legal'),
-//     }
-// };
 
 // npx webpack to run
